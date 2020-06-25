ee1947a283df00dddc1a4729f0baf9f793843aee
Gaurav Goyal <gaurav.goyal@acquia.com>
2020-06-25 11:31:04 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Description
+This is a fairly simple node js implementation and more of a boilerplate, which aims to automate the scenarios for and regenerates the backstop.json file with default configurations.
+
+This generator automates the generation of scenarios when you want to cover multiple links across multiple websites. For e.g. if you want to run visual regression on all the links under main menu, this generator will automatically generate the scenarios for the links under main menu.
+
+## Installation steps
+* Install latest version of node.js. Download it from [here](https://nodejs.org/en/download/)
+* Download this repository, and goto backstop-generator folder
+* Run `npm install`
+* Create a `.env` file inside the folder.
+   * `REFERENCE_URL` - Give the absolute URL. The screenshots from this URL will be used as base URL.
+   * `TEST_URL` - Give the absolute test URL. If this url is behind a htaccess, pass the credentials in the URL.
+   * `LINK_SELECTOR` - Give a selector which will be used to grab all the href tests, to run visual regression tests. Keeping it empty would take all the anchor tags from the `REFERENCE_URL`.
+* Run `npm run default` - it will generate the default configurations needed to run backstopJS.
+* Run `npm run generate` - it will generate and update the `backstop.json` file with all the scenarios.
+* Run `npm run reference` to generate the screenshots for reference websites.
+* Run `npm run comparision` to generate a comparision report.
+
+## Updating configurations
+Once backstop js generates the configuration, its role is finished. If you want to change any of the configuration within scnearios, those should happen inside backstop.json or you can change backstopjs-generator.js.
+
+## Credits
+Thanks to [BackstopJS](https://github.com/garris/BackstopJS) for creating this wonderful tool.
---
+++ backstopjs-generator.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+const $ = require('cheerio');
+const rp = require('request-promise');
+const fs = require('fs');
+
+const url = process.env.REFERENCE_URL;
+const testUrl = process.env.TEST_URL;
+const selector = process.env.LINK_SELECTOR || 'a';
+
+rp(url).then(html => {
+  const linkObjects = $(selector, html);
+  const links = [];
+  // we only need the "href" and "title" of each link
+
+  linkObjects.each(function (index, element) {
+    let title = $(element).text().replace(/(\r\n|\n|\r)/gm, "").trim();
+    if (title !== undefined && title != '' &&  element.attribs.href != '#' && !links.some(el => el.href === element.attribs.href)) {
+      links.push({
+          href: element.attribs.href,
+          title: title
+      });
+    }
+  })
+
+  const scenarios = [];
+
+  links.forEach(function(element) {
+    scenarios.push({
+      "label": element.title,
+      "url": testUrl + element.href,
+      "referenceUrl": url + element.href,
+      "delay": 2000,
+      "cookiePath": "backstop_data/engine_scripts/cookies.json",
+      "readyEvent": "",
+      "readySelector": "",
+      "delay": 0,
+      "hideSelectors": [],
+      "removeSelectors": [],
+      "hoverSelector": "",
+      "clickSelector": "",
+      "postInteractionWait": 0,
+      "selectors": [],
+      "selectorExpansion": true,
+      "expect": 0,
+      "misMatchThreshold" : 0.1,
+      "requireSameDimensions": true
+    });
+  })
+
+  // Generate backstop.json file.
+  var json = {
+    "viewports": [
+      {
+        "label": "desktop",
+        "width": 1368,
+        "height": 1600
+      }
+    ],
+    "onBeforeScript": "puppet/onBefore.js",
+    "onReadyScript": "puppet/onReady.js",
+    "scenarios": scenarios,
+    "paths": {
+      "bitmaps_reference": "backstop_data/bitmaps_reference",
+      "bitmaps_test": "backstop_data/bitmaps_test",
+      "engine_scripts": "backstop_data/engine_scripts",
+      "html_report": "backstop_data/html_report",
+      "ci_report": "backstop_data/ci_report"
+    },
+    "report": ["browser"],
+    "engine": "puppeteer",
+    "engineOptions": {
+      "args": ["--no-sandbox"]
+    },
+    "asyncCaptureLimit": 5,
+    "asyncCompareLimit": 50,
+    "debug": true,
+    "debugWindow": false,
+  };
+  let data = JSON.stringify(json);
+  fs.writeFileSync('backstop.json', data);
+
+})
+.catch(err => {
+    console.log(err);
+})
